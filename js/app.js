1bb33fcfa2e971db395e721b720012b6d64a6027
raging04 <gui.soares97@gmail.com>
2026-06-29 10:57:39 +0100

    Minor adjustments and added JSDoc

--- js/app.js
+++ js/app.js
@@ -19,12 +19,38 @@ const calculator = {
   }
 }
 
-let display = document.getElementById("display");
+/**
+ *
+ * Display of the calculator. This is where the program gets the full user input.
+ *
+ * @type {HTMLElement}
+ */
+const display = document.getElementById("display");
+
 let number;
 let operator = "";
+
+/**
+ *
+ * Condition to enable the equal button if a number is on display.
+ *
+ * @type {boolean}
+ */
 let in_operation = false;
+
+/**
+ *
+ * Condition to lock the operator and first number.
+ *
+ *
+ * @type {boolean}
+ */
 let operation_pressed = false;
 
+
+/**
+ *  Keyboard support for the calculator.
+ */
 addEventListener('keydown', function(e) {
   if (e.code.includes("Digit")) {
     appendNumber(e.key)
@@ -33,18 +59,35 @@ addEventListener('keydown', function(e) {
   }
 })
 
+/**
+ * Clear the display.
+ */
 function clearDisplay() {
   display.textContent = "";
 }
 
+/**
+ * Backspace
+ */
 function clearLeft() {
   display.textContent = display.textContent.slice(0,-1)
 }
 
+/**
+ * Check and disables period input if another period is already inserted.
+ */
 function checkDecimal() {
   document.getElementById("point").disabled = display.textContent.includes('.');
 }
 
+
+/**
+ *
+ * Appends a number, in form of string, to the display
+ *
+ *
+ * @param num {string} Number to append
+ */
 function appendNumber(num) {
   if(!in_operation) {
     clearDisplay();
@@ -55,10 +98,23 @@ function appendNumber(num) {
   checkDecimal()
 }
 
+/**
+ *
+ * Gets and converts the display string into integer
+ *
+ * @returns {number} Display number in integer
+ */
 function getNumber() {
    return parseInt(display.textContent);
 }
 
+
+/**
+ *
+ * Gets the first number and operator picked by the user. Updates the global variables
+ *
+ * @param operation {string} Operation to execute.
+ */
 function getOperator(operation) {
   if(!operation_pressed){
     operator = operation;
@@ -69,13 +125,16 @@ function getOperator(operation) {
 
 }
 
-
+/**
+ *
+ * Executes the operation, given the numbers and operator. Displays the results and readies the calculator for another operation.
+ *
+ *
+ * @param num1 {integer} First number of the operation, already converted to int. Default value is the global variable number
+ * @param num2 {string} Second number of the operation, in string. Is converted inside the function.
+ * @param operation {string} Operation to execute. Default is global variable operator
+ */
 function operate(num1 = number, num2, operation = operator) {
-  console.log(operator);
-  console.log(num1)
-  console.log(num2)
-  num2 = parseInt(num2);
-
   switch(operation) {
     case "plus":
       display.textContent = calculator.add(num1, parseInt(num2)).toString();
@@ -87,7 +146,7 @@ function operate(num1 = number, num2, operation = operator) {
       display.textContent = calculator.multiply(num1, parseInt(num2)).toString();
         break;
     case "division":
-      if(num2 === 0){
+      if(parseInt(num2) === 0){
         alert("You cant divide by zero");
         clearDisplay();
       } else {
@@ -95,7 +154,7 @@ function operate(num1 = number, num2, operation = operator) {
         break;
       }
   }
-  //Reset
+
   in_operation = false;
   operation_pressed = false;
   document.getElementById("equal").disabled = true;
